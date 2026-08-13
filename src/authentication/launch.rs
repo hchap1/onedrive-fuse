63fe17a305d42b1fa0a8ccd47ebd0d2608ed4d37
Harrison Chapman <hchap1@hotmail.com>
2026-08-13 15:23:39 +1000

    fix: URL safe encoding

--- src/authentication/launch.rs
+++ src/authentication/launch.rs
@@ -1,5 +1,6 @@
 use crate::error::Res;
 use crate::authentication::callback::CALLBACK_PORT;
+use url::Url;
 
 // oauth2
 const URL: &str = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
@@ -22,20 +23,20 @@ const CODE_CHALLENGE_METHOD: &str = "S256";
 
 /// Non-blocking call opening oauth2 permission page in default browser
 pub fn launch_oauth2(csrf: String, pkce: String) -> Res<()> {
-
     let redirect_uri = format!("{REDIRECT_URI}:{}", CALLBACK_PORT);
-    open::that(
-        format!(
-            "{URL}?\
-            client_id={CLIENT_ID}&\
-            response_type={RESPONSE_TYPE}&\
-            redirect_uri={redirect_uri}&\
-            response_mode={RESPONSE_MODE}&\
-            scope={SCOPE}&\
-            state={csrf}&\
-            code_challenge={pkce}&\
-            code_challenege_method={CODE_CHALLENGE_METHOD}"
-        )
-    )?;
+
+    let mut auth_url = Url::parse(URL)?;
+    auth_url
+        .query_pairs_mut()
+        .append_pair("client_id", CLIENT_ID)
+        .append_pair("response_type", RESPONSE_TYPE)
+        .append_pair("redirect_uri", &redirect_uri)
+        .append_pair("response_mode", RESPONSE_MODE)
+        .append_pair("scope", SCOPE)
+        .append_pair("state", &csrf)
+        .append_pair("code_challenge", &pkce)
+        .append_pair("code_challenge_method", CODE_CHALLENGE_METHOD);
+
+    open::that(auth_url.as_str())?;
     Ok(())
 }
--- src/authentication/mod.rs
+++ src/authentication/mod.rs
@@ -1,3 +1,5 @@
 pub mod launch;
 pub mod callback;
 pub mod tokens;
+pub mod authenticate;
+pub mod security;
