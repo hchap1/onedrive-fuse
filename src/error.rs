61dea2bf5d357318a39095ba14517d4ce095abdb
Harrison Chapman <hchap1@hotmail.com>
2026-08-13 15:24:41 +1000

    fix: Error case for URL encoding error

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,10 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("IO error {:?}", .0)]
-    IoError(#[from] std::io::Error)
+    IoError(#[from] std::io::Error),
+
+    #[error("URL Parse Error {:?}", .00)]
+    UrlError(#[from] url::ParseError),
 }
 
 pub type Res<T> = Result<T, Error>;
