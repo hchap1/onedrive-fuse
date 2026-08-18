338aace081f7b82df2bccebe0179d8a951d1a54f
Harrison Chapman <hchap1@hotmail.com>
2026-08-18 22:59:56 +1000

    feat: axum receives POST request

--- src/authentication/callback.rs
+++ src/authentication/callback.rs
@@ -1,1 +1,27 @@
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use axum::routing::post;
+use axum::http::StatusCode;
+
+use axum::Json;
+use axum::Router;
+
+use crate::error::Res;
+
 pub const CALLBACK_PORT: u16 = 3283;
+
+pub async fn callback() -> Res<()> {
+    let app = Router::new()
+        .route("/", post(handle_post));
+
+    let listener = tokio::net::TcpListener::bind((IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), CALLBACK_PORT))
+        .await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+pub async fn handle_post(thing: String) -> (StatusCode, Json<()>) {
+    println!("Returned: {thing}");
+    (StatusCode::ACCEPTED, Json(()))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 pub mod authentication;
 pub mod error;
 
+use crate::error::Res;
+
+use crate::authentication::launch::launch_oauth2;
+use crate::authentication::callback::callback;
+use crate::authentication::security::generate_csrf;
+use crate::authentication::security::generate_pkce;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Res<()> {
+   let (verifier, challenge) = generate_pkce(); 
+   let state = generate_csrf();
+
+   launch_oauth2(state, challenge)?;
+   callback().await?;
+   Ok(())
 }
